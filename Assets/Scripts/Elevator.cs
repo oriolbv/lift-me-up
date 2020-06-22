ae82a57a270216a0a0a004e21deb56b1c9e8ad10
Oriol Burgaya <oriolbv@gmail.com>
2020-06-22 22:40:17 +0200

    Add force when pressing space

--- Assets/Scripts/Elevator.cs
+++ Assets/Scripts/Elevator.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class Elevator : MonoBehaviour
 {
+
+    private Rigidbody2D rb;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = this.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space)) 
+        {
+            Vector3 forceApplied = new Vector3(10, 10 , 10);
+     
+            rb.AddForce(forceApplied);
+        }
     }
 }
