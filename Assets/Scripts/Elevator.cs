e1aa4ea56e1d6fb16d0df01a2492e69a6c9f7760
Oriol Burgaya <oriolbv@gmail.com>
2020-06-23 23:15:03 +0200

    Translate movement added

--- Assets/Scripts/Elevator.cs
+++ Assets/Scripts/Elevator.cs
@@ -6,19 +6,26 @@ public class Elevator : MonoBehaviour
 {
 
     private Rigidbody2D rb;
+    private Transform transform;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = this.GetComponent<Rigidbody2D>();
+        transform = this.GetComponent<Transform>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space)) 
-        {    
-            rb.AddForce(new Vector2(0f, transform.up.y * 200), ForceMode2D.Force);
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            transform.Translate(new Vector2(0, 5f) * Time.deltaTime);
         }
+        
+        // if () 
+        // {    
+        //     // transform.position = new Vector2(transform.position.x, transform.position.y + 1f);
+        //     // rb.AddForce(new Vector2(0f, transform.up.y * 200), ForceMode2D.Force);
+        // }
     }
 }
