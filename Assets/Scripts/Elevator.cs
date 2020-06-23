745d7c25070b62e0955851d44af63442ed6312d0
Oriol Burgaya <oriolbv@gmail.com>
2020-06-24 00:01:16 +0200

    Using FixedUpdate in order to move elevator

--- Assets/Scripts/Elevator.cs
+++ Assets/Scripts/Elevator.cs
@@ -8,24 +8,34 @@ public class Elevator : MonoBehaviour
     private Rigidbody2D rb;
     private Transform transform;
 
+    private bool gas;
+
     // Start is called before the first frame update
     void Start()
     {
         rb = this.GetComponent<Rigidbody2D>();
         transform = this.GetComponent<Transform>();
+        gas = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space)) {
+        if (Input.GetKey(KeyCode.Space)) {
+            Debug.Log("UP");
+            gas = true;
+        }
+        else {
+            Debug.Log("DOWN");
+            gas = false;
+        }
+
+        
+    }
+
+    private void FixedUpdate() {
+        if (gas) {
             transform.Translate(new Vector2(0, 5f) * Time.deltaTime);
         }
-        
-        // if () 
-        // {    
-        //     // transform.position = new Vector2(transform.position.x, transform.position.y + 1f);
-        //     // rb.AddForce(new Vector2(0f, transform.up.y * 200), ForceMode2D.Force);
-        // }
     }
 }
