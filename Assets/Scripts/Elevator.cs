a2ea3071ccdad9faa6912bbe9bddd16ee3da8b0f
Oriol Burgaya <oriolbv@gmail.com>
2020-06-24 22:11:20 +0200

    Elevator lifting correctly

--- Assets/Scripts/Elevator.cs
+++ Assets/Scripts/Elevator.cs
@@ -8,6 +8,7 @@ public class Elevator : MonoBehaviour
     private Rigidbody2D rb;
 
     private bool gas;
+    private bool gas2;
 
     public Transform StartMarker;
     public Transform EndMarker;
@@ -25,6 +26,7 @@ public class Elevator : MonoBehaviour
     {
         rb = this.GetComponent<Rigidbody2D>();
         gas = false;
+        gas2 = false;
 
          // Keep a note of the time the movement started.
         startTime = Time.time;
@@ -50,7 +52,25 @@ public class Elevator : MonoBehaviour
 
     private void FixedUpdate() {
         if (gas) {
-            transform.Translate (Vector3.up * Time.deltaTime * 4f);
+            if (gas2) {
+                // Keep a note of the time the movement started.
+                startTime = Time.time;
+                // Calculate the journey length.
+                journeyLength = Vector3.Distance(StartMarker.position, EndMarker.position);
+                
+                gas2 = false;
+            }
+            
+            // Distance moved equals elapsed time times speed..
+            float distCovered = (Time.time - startTime) * speed;
+            // Fraction of journey completed equals current distance divided by total distance.
+            float fractionOfJourney = distCovered / journeyLength;
+
+            // Set our position as a fraction of the distance between the markers.
+            transform.position = Vector3.Lerp(StartMarker.position, EndMarker.position, fractionOfJourney);
+        }
+        else {
+            gas2 = true;
         }
     }
 }
