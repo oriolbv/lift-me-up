c015106dd22938f4259dacbe3958a906fe00ed7a
Oriol Burgaya <oriolbv@gmail.com>
2020-06-24 21:47:09 +0200

    StartMarker and EndMarker added

--- Assets/Scripts/Elevator.cs
+++ Assets/Scripts/Elevator.cs
@@ -6,16 +6,31 @@ public class Elevator : MonoBehaviour
 {
 
     private Rigidbody2D rb;
-    private Transform transform;
 
     private bool gas;
+
+    public Transform StartMarker;
+    public Transform EndMarker;
+
+    public float speed = 1.0F;
+    
+    // Time when the movement started.
+    private float startTime;
+
+    // Total distance between the markers.
+    private float journeyLength;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = this.GetComponent<Rigidbody2D>();
-        transform = this.GetComponent<Transform>();
         gas = false;
+
+         // Keep a note of the time the movement started.
+        startTime = Time.time;
+
+        // Calculate the journey length.
+        journeyLength = Vector3.Distance(StartMarker.position, EndMarker.position);
     }
 
     // Update is called once per frame
@@ -35,7 +50,7 @@ public class Elevator : MonoBehaviour
 
     private void FixedUpdate() {
         if (gas) {
-            transform.Translate(new Vector2(0, 5f) * Time.deltaTime);
+            transform.Translate (Vector3.up * Time.deltaTime * 4f);
         }
     }
 }
