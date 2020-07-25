fa36055f5efa09af619a862616eeaf2ba9de5166
Oriol Burgaya <oriolbv@gmail.com>
2020-07-25 14:25:12 +0200

    First building prototype added into the scene

--- Assets/Scripts/CameraFollower.cs
+++ Assets/Scripts/CameraFollower.cs
@@ -6,16 +6,18 @@ public class CameraFollower : MonoBehaviour
 {
     public GameObject targetObject;
 
+    private float distanceToTarget;
+
     void Start()
     {
-        
+        distanceToTarget = transform.position.y - targetObject.transform.position.y;
     }
 
     void Update()
     {
         float targetObjectY = targetObject.transform.position.y;
         Vector3 newCameraPosition = transform.position;
-        newCameraPosition.y = targetObjectY;
+        newCameraPosition.y = targetObjectY + distanceToTarget;
         transform.position = newCameraPosition;
     }
 }
