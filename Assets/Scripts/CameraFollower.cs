bab1e49f2e4a28b38216c34be4b4f54530f58602
Oriol Burgaya <oriolbv@gmail.com>
2020-07-25 13:37:22 +0200

    Camera follower script added and useless gameobjects removed from scene

---
+++ Assets/Scripts/CameraFollower.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollower : MonoBehaviour
+{
+    public GameObject targetObject;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        float targetObjectY = targetObject.transform.position.y;
+        Vector3 newCameraPosition = transform.position;
+        newCameraPosition.y = targetObjectY;
+        transform.position = newCameraPosition;
+    }
+}
